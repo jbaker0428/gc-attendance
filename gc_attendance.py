1a27ea82aa18b0285cc1092bb87179d754080dc3
Joe Baker <jbaker0428@gmail.com>
2012-04-07 19:42:11 -0400

    Excuse, Signin, and Event static class method stubs.
    Signed-off-by: Joe Baker <jbaker0428@gmail.com>

--- gc_attendance.py
+++ gc_attendance.py
@@ -40,6 +40,9 @@ def createTables():
 		con.close()
 
 class Student:
+	''' A Student who has signed into the attendance system. 
+	The student's RFID ID number is the primary key column.'''
+	
 	@staticmethod
 	def select_by_id(id):
 		''' Return the Student of given ID. '''
@@ -75,8 +78,6 @@ class Student:
 		''' Return the list of Students taking Glee Club for credit (or not). '''
 		pass
 	
-	''' A Student who has signed into the attendance system. 
-	The student's RFID ID number is the primary key column.'''
 	def __init__(self, r, fn, ln, email, shm=False, officer=False, standing=True, cred=False):
 		self.rfid = r		# Numeric ID seen by the RFID reader
 		self.fname = fn
@@ -114,7 +115,18 @@ class Student:
 	
 class Excuse:
 	''' A Student's excuse for missing an Event sent to gc-excuse. 
-	The datetime and student ID are the primary key colums.''' 
+	The datetime and student ID are the primary key colums.'''
+	
+	@staticmethod
+	def select_by_student(id):
+		''' Return the list of Excuses by a Student. '''
+		pass
+	
+	@staticmethod
+	def select_by_date(start_date, end_date):
+		''' Return the list of Excuses in a given datetime range. '''
+		pass
+	 
 	def __init__(self, dt, r, s):
 		self.excuse_date = dt	# a datetime object
 		self.reason = r		# Student's message to gc-excuse
@@ -138,6 +150,17 @@ class Excuse:
 class Signin:
 	''' Corresponds to a row in the RFID output file. 
 	The datetime and student ID are the primary key colums.'''
+	
+	@staticmethod
+	def select_by_student(id):
+		''' Return the list of Signins by a Student. '''
+		pass
+	
+	@staticmethod
+	def select_by_date(start_date, end_date):
+		''' Return the list of Signins in a given datetime range. '''
+		pass
+	
 	def __init__(self, dt, s):
 		self.signin_date = dt	# a datetime object
 		self.student = s
@@ -163,6 +186,16 @@ class Event:
 	TYPE_REHEARSAL = 'Rehearsal'
 	TYPE_DRESS = 'Dress Rehearsal'	# Mandatory for a concert
 	TYPE_CONCERT = 'Concert'
+	
+	@staticmethod
+	def select_by_date(start_date, end_date):
+		''' Return the list of Events in a given datetime range. '''
+		pass
+	
+	@staticmethod
+	def select_by_type(type):
+		''' Return the list of Events of a given type. '''
+		pass
 	
 	def __init__(self, dt, t):
 		self.event_date = dt	# a datetime object, primary key
