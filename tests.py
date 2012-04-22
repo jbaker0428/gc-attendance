fc1508e1dfe29dad5374686f54462d7064d2629f
Joe Baker <jbaker0428@gmail.com>
2012-04-22 18:25:33 -0400

    Initial setup/teardown for unit testing.
    Signed-off-by: Joe Baker <jbaker0428@gmail.com>

---
+++ tests.py
@@ -0,0 +1,51 @@
+import os
+import unittest
+import datetime
+from gc_attendance import *
+
+class AttendanceTestCase(unittest.TestCase):
+	def setUp(self):
+		unittest.TestCase.setUp(self)
+		self.db = AttendanceDB(os.path.join(os.getcwd(), 'dbtests.sqlite'))
+		self.glee_club = Organization('glee_club')
+		self.shm = Organization('SHM')
+		self.a_term = Term('A11', datetime.date(2011, 8, 25), datetime.date(2011, 10, 13), [datetime.date(2011, 9, 5)])
+		self.b_term = Term('B11', datetime.date(2011, 10, 25), datetime.date(2011, 12, 15), [datetime.date(2011, 11, 23), datetime.date(2011, 11, 24), datetime.date(2011, 11, 25)])
+		self.fall_semester = Semester('fall_2011', self.a_term, self.b_term)
+		self.gc_group = Group(None, self.glee_club, self.fall_semester)
+		self.shm_group = Group(None, self.shm, self.fall_semester)
+		self.student10000 = Student(10000, 'Shawn', 'Onessimo', 'smonessimo@wpi.edu')
+		self.student11262 = Student(11262, 'Andrew', 'St. Jean', 'astjean@wpi.edu')
+		self.student12345 = Student(12345, 'Ryan', 'Staunch', 'rstaunch@wpi.edu')
+		self.student11959 = Student(11959, 'John', 'Delorey', 'jfd@wpi.edu')
+		self.student12011 = Student(12011, 'Tony', 'Guerra', 'aguerra@wpi.edu')
+		self.student42000 = Student(42000, 'José', 'Navedo', 'jnavedo@wpi.edu')
+		self.student56789 = Student(56789, 'Travis', 'Briggs', 'tbriggs@wpi.edu')
+		self.student42737 = Student(42737, 'Joe', 'Baker', 'jbaker@alum.wpi.edu')
+		self.student24631 = Student(24631, 'Anika', 'Blodgett', 'ablodgett@wpi.edu')
+		
+		self.rehearsal1 = Event(None, 'Rehearsal', datetime.datetime(2011, 9, 6, 18, 30), Event.TYPE_REHEARSAL, self.gc_group, self.fall_semester)
+		self.signin_r1_10000 = Signin(datetime.datetime(2011, 9, 6, 18, 28), None, self.student10000)
+		self.signin_r1_11262 = Signin(datetime.datetime(2011, 9, 6, 18, 28), None, self.student11262)
+		self.signin_r1_12345 = Signin(datetime.datetime(2011, 9, 6, 18, 30), None, self.student12345)
+		self.signin_r1_11959 = Signin(datetime.datetime(2011, 9, 6, 18, 32), None, self.student11959)
+		self.signin_r1_12011 = Signin(datetime.datetime(2011, 9, 6, 18, 31), None, self.student12011)
+		self.signin_r1_42000 = Signin(datetime.datetime(2011, 9, 6, 18, 34), None, self.student42000)
+		self.signin_r1_56789 = Signin(datetime.datetime(2011, 9, 6, 20, 12), None, self.student56789)
+		self.signin_r1_42737 = Signin(datetime.datetime(2011, 9, 6, 18, 35), None, self.student42737)
+		
+		self.dress1 = Event(None, 'Rehearsal for September 11', datetime.datetime(2011, 9, 10, 10, 00), Event.TYPE_DRESS, self.gc_group, self.fall_semester)
+		self.concert1 = Event(None, 'Memorial Mass', datetime.datetime(2011, 9, 11, 12, 00), Event.TYPE_CONCERT, self.gc_group, self.fall_semester)
+		self.rehearsal2 = Event(None, 'Rehearsal', datetime.datetime(2011, 9, 13, 18, 30), Event.TYPE_REHEARSAL, self.gc_group, self.fall_semester)
+		self.rehearsal3 = Event(None, 'Rehearsal', datetime.datetime(2011, 9, 20, 18, 30), Event.TYPE_REHEARSAL, self.gc_group, self.fall_semester)
+		self.rehearsal4 = Event(None, 'Rehearsal', datetime.datetime(2011, 9, 27, 18, 30), Event.TYPE_REHEARSAL, self.gc_group, self.fall_semester)
+		self.rehearsal5 = Event(None, 'Rehearsal', datetime.datetime(2011, 10, 4, 18, 30), Event.TYPE_REHEARSAL, self.gc_group, self.fall_semester)
+		self.rehearsal6 = Event(None, 'Rehearsal', datetime.datetime(2011, 10, 11, 18, 30), Event.TYPE_REHEARSAL, self.gc_group, self.fall_semester)
+		
+	def tearDown(self):
+		unittest.TestCase.tearDown(self)
+		del self.db
+		os.remove(os.path.join(os.getcwd(), 'dbtests.sqlite'))
+
+if __name__ == '__main__':
+	unittest.main()	
